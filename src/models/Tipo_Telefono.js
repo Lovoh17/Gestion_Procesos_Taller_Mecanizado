4d5338bbc15b99261a8b885698ea440462126cd7
Elias Esperanza <eliasesperanza261380@gmail.com>
2025-04-16 21:31:50 -0600

    ADD modelos

---
+++ src/models/Tipo_Telefono.js
@@ -0,0 +1,22 @@
+import { sequelize } from "../database/database.js";
+import { DataTypes } from "sequelize";
+
+export const Tipo_Telefono = sequelize.define("tipos_telefono", {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descripcion:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+},{
+    timestamps: false,
+    tableName: "tipos_telefono"
+});
