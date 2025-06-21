8e1e6be796554512561c2c0b39739c967d7571ad
Gabriel De la O <GabrielDelaO235@gmail.com>
2025-06-20 23:56:45 -0600

    la rutas completas

--- src/modules/Usuario/Usuario.routes.js
+++ src/modules/Usuario/Usuario.routes.js
@@ -7,6 +7,6 @@ router.get("/Usuario", obtenerUsuarios);
 router.get("/Usuario/:id", obtenerUsuarioPorId);
 router.put("/Usuario/:id", actualizarUsuario);
 router.delete("/Usuario/:id", eliminarUsuario);
-router.get("/Usuario/Puesto/:puesto_id")
+router.get("/Usuario/Puesto/:puesto_id", obtenerUsuariosPorIdPuesto);
 
 export default router;
